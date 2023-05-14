b52faa74027209241300db8d818f8b5efee2142e
Vadiraj Purohit <61923084+cricketbackground@users.noreply.github.com>
2023-05-14 17:02:51 -0400

    Add files via upload

---
+++ src/main/java/com/example/docs/SwaggerConfiguration.java
@@ -0,0 +1,21 @@
+package com.example.docs;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI baseOpenAPI(){
+        return new OpenAPI().info(
+                new Info()
+                        .title("This is a test")
+                        .version("0.1")
+        );
+    }
+}
